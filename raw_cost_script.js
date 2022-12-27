fca3946a58fdb1dfa17d1b405cc1c5fe00df9f70
piotrekkk23 <gppiotrekgp23@gmail.com>
2022-12-27 18:06:40 +0100

    calculating cost of one page

---
+++ raw_cost_script.js
@@ -0,0 +1,28 @@
+function calculateCosts(){
+    let pname = document.getElementById("printer").value;
+    let bcount = parseInt(document.getElementById("blackCount").value);
+    let ccount = parseInt(document.getElementById("colorCount").value);
+    let bcost = parseInt(parseFloat(document.getElementById("blackCost").value)*100);
+    let ccost = parseInt(parseFloat(document.getElementById("cyanCost").value)*100);
+    let mcost = parseInt(parseFloat(document.getElementById("magentaCost").value)*100);
+    let ycost = parseInt(parseFloat(document.getElementById("yellowCost").value)*100);
+
+    let summaryText = "Printing costs in printer " + pname + " are:<br>";
+
+    if(bcount <= 0 || ccount <= 0 || bcost <= 0 || ccost <= 0 || mcost <= 0 || ycost <= 0){
+        alert("Error, wrong input values");
+        document.getElementById("summaryText").innerHTML = "The value you have filled are wrong, fix them to get correct results.";
+    }
+    else if (bcount && bcost ){
+        let blackPageCost = (bcost/bcount)/100;
+        summaryText += "Printing only in black will cost you " + blackPageCost + " of your currency per page.<br>";
+        document.getElementById("summary").style.visibility = "visible";
+        document.getElementById("summaryText").innerHTML = summaryText;
+        if (ccount && ccost && mcost && ycost){
+            let colorPageCost = ((ccost+mcost+ycost)/ccount)/100 + blackPageCost;
+            summaryText += "Printing in color will cost you " + colorPageCost + " of your currency per page.<br>";
+            document.getElementById("summaryText").innerHTML = summaryText;
+        }
+    }
+
+}
